e5346cc60f929ebb0743cb8db472975f97b779c7
sudongdong <sudongdong@chainup.com>
2020-10-22 18:32:25 +0800

    Updates

---
+++ precache-manifest.9d704da95ee13aa3c013a019b30d8631.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "286447f65c22657ebd174cc4af97cc43",
+    "url": "/bigrichman.github.io/index.html"
+  },
+  {
+    "revision": "99b801d56b5eb4b9436b",
+    "url": "/bigrichman.github.io/static/css/main.04d54a83.chunk.css"
+  },
+  {
+    "revision": "f030033ac5af4c4583f6",
+    "url": "/bigrichman.github.io/static/js/2.4b47b73b.chunk.js"
+  },
+  {
+    "revision": "6c19783ced6bae75444858dab9dff6e1",
+    "url": "/bigrichman.github.io/static/js/2.4b47b73b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "99b801d56b5eb4b9436b",
+    "url": "/bigrichman.github.io/static/js/main.1942fa79.chunk.js"
+  },
+  {
+    "revision": "a1df4c07c92ebf063510",
+    "url": "/bigrichman.github.io/static/js/runtime-main.1808eb34.js"
+  },
+  {
+    "revision": "fce8c82bf5aa7916e885b3f2f9f67e39",
+    "url": "/bigrichman.github.io/static/media/homebg.fce8c82b.png"
+  },
+  {
+    "revision": "64a6f16c4f5ef7e189e75dfc0a52a25f",
+    "url": "/bigrichman.github.io/static/media/homecard@2x.64a6f16c.png"
+  },
+  {
+    "revision": "41236031fc933b053717ef1760601e36",
+    "url": "/bigrichman.github.io/static/media/start_btn.41236031.png"
+  },
+  {
+    "revision": "2a4a2235f6ed0e838b49a3f1bec4aae1",
+    "url": "/bigrichman.github.io/static/media/tester-bg@2x.2a4a2235.png"
+  },
+  {
+    "revision": "08087132d6dcfcd5c5f471880c104712",
+    "url": "/bigrichman.github.io/static/media/zombierun.08087132.png"
+  }
+]);
